dd2c925559a51a50e27eb493ed5e667020c1efaf
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-02-03 18:45:41 -0500

    make array two dimensional, check for horizontal and vertical wins

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const gameBoard = (() => {
 
-  const board = [];
+  let board = [
+    // Rows
+    [],
+    [],
+    []
+  ];
 
   let currPlayer = 'X';
   const squares = document.querySelectorAll('.game-square');
@@ -14,21 +19,73 @@ const gameBoard = (() => {
   
   function makeSelection(e) {
     const square = e.target;
-    const index = square.dataset.index;
+    const row = square.dataset.row;
+    const column = square.dataset.column;
+
     if (square.textContent == '') {
       square.textContent = currPlayer;
-      board[index] = currPlayer;
+      board[row][column] = currPlayer;
       square.classList.add('selected');
 
-      currPlayer == 'X' ? currPlayer = 'O' : currPlayer = 'X';
+      const gameOver = checkWins(currPlayer);
+      if (!gameOver) {
+        rotatePlayer();
+      } else {
+        clearBoard();
+      }
     }
   }
 
   function clearBoard() {
     squares.forEach(square => {
       square.textContent = '';
+      board = [[], [], []];
       currPlayer = 'X';
+
+      if (square.classList.contains('selected')) {
+        square.classList.remove('selected');
+      }
     });
+  }
+
+  function checkWins(player) {
+    let usedSquares = 0;
+
+    for (let i = 0; i < 3; i++) {
+
+      // Check row win
+      let rowCount = 0;
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === player) {
+          rowCount++;
+        }
+      }
+      if (rowCount === 3) {
+        alert(`player ${player} wins!`);
+        return true;
+      }
+
+      // Check column win
+      const col1 = board[0][i];
+      const col2 = board[1][i];
+      const col3 = board[2][i];
+
+      if (col1 === player && col2 === player && col3 === player) {
+        alert(`player ${player} wins!`);
+        return true;
+      }
+    }
+    // Check for draw
+    if (usedSquares === 9) {
+      alert(`It's a draw!`);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function rotatePlayer() {
+    currPlayer == 'X' ? currPlayer = 'O' : currPlayer = 'X';
   }
 
 })();
