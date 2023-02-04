2b47229b6c596adca60f7f7b4098b9e0cefb0beb
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-02-03 20:55:06 -0500

    check diagonal wins

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const gameBoard = (() => {
   ];
 
   let currPlayer = 'X';
-  const squares = document.querySelectorAll('.game-square');
+  const squares = document.querySelectorAll('#game-square');
   const resetButton = document.querySelector('#reset');
   
   squares.forEach(square => {
@@ -51,6 +51,19 @@ const gameBoard = (() => {
   function checkWins(player) {
     let usedSquares = 0;
 
+    // Check diagonal win
+    if (board[0][0] === player) {
+      if (board[1][1] === player && board[2][2] === player) {
+        alert(`player ${player} wins!`);
+        return true;
+      }
+    } else if (board[2][0] === player) {
+      if (board[1][1] === player && board[0][2] === player) {
+        alert(`player ${player} wins!`);
+        return true;
+      }
+    }
+
     for (let i = 0; i < 3; i++) {
 
       // Check row win
@@ -58,6 +71,9 @@ const gameBoard = (() => {
       for (let j = 0; j < 3; j++) {
         if (board[i][j] === player) {
           rowCount++;
+        }
+        if (board[i][j] != undefined) {
+          usedSquares++;
         }
       }
       if (rowCount === 3) {
@@ -69,12 +85,12 @@ const gameBoard = (() => {
       const col1 = board[0][i];
       const col2 = board[1][i];
       const col3 = board[2][i];
-
       if (col1 === player && col2 === player && col3 === player) {
         alert(`player ${player} wins!`);
         return true;
       }
     }
+
     // Check for draw
     if (usedSquares === 9) {
       alert(`It's a draw!`);
@@ -87,7 +103,4 @@ const gameBoard = (() => {
   function rotatePlayer() {
     currPlayer == 'X' ? currPlayer = 'O' : currPlayer = 'X';
   }
-
 })();
-
-
